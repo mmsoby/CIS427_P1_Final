81f142fd0e14182d24abaaf001c51f65f79bc2e8
Mustafa Soby <mustafasoby@icloud.com>
2022-02-12 20:24:45 -0500

    Fixed parse int exception

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -21,19 +21,19 @@ public class Server {
     public static void main(String[] args) {
         loadUsers();
         boolean result;
-        do{
+        do {
             //result stores the value of the createCommunicationLoop() so that after
             //someone logs out the server still runs.
             result = createCommunicationLoop();
-        } while(result);
+        } while (result);
     }//end main
 
     //This function loads all the registered users into an arraylist from the file
-    public static void loadUsers(){
+    public static void loadUsers() {
         try {
             Scanner sc = new Scanner(new File(USERS_FILENAME));
             sc.useDelimiter("\n");   //sets the delimiter pattern
-            while (sc.hasNext()){
+            while (sc.hasNext()) {
                 logins.add(sc.nextLine());
             }
         } catch (FileNotFoundException e) {
@@ -58,24 +58,24 @@ public class Server {
 
             //server loop listening for the client
             //and responding
-            while(true) {
+            while (true) {
                 String strReceived = inputFromClient.readUTF();
                 System.out.println(strReceived);
 
-                String [] inputArray = strReceived.split(" ");
+                String[] inputArray = strReceived.split(" ");
 
-                if(inputArray[0].equalsIgnoreCase("LOGIN")) {
-                    if(isLoggedIn){
+                if (inputArray[0].equalsIgnoreCase("LOGIN")) {
+                    if (isLoggedIn) {
                         outputToClient.writeUTF("You must first logout.");
                         continue;
                     }
 
                     //Check login info and send back corresponding message
-                    if(inputArray.length == 3){
+                    if (inputArray.length == 3) {
                         String userEntry = inputArray[1] + " " + inputArray[2];
 
-                        if(logins.contains(userEntry)){
-                            System.out.println("User " +  inputArray[1] + " logged in.");
+                        if (logins.contains(userEntry)) {
+                            System.out.println("User " + inputArray[1] + " logged in.");
                             outputToClient.writeUTF("SUCCESS");
                             isLoggedIn = true;
                             currentUser = inputArray[1];
@@ -91,17 +91,17 @@ public class Server {
 
                 }
 
-                else if(inputArray[0].equalsIgnoreCase("SOLVE")){
-                    if(isLoggedIn){
+                else if (inputArray[0].equalsIgnoreCase("SOLVE")) {
+                    if (isLoggedIn) {
                         String dimensions = calculateDimensions(inputArray);
                         outputToClient.writeUTF(dimensions);
 
                         //Create file if not already created
-                        File usersFile = new File("files/"+currentUser+".txt");
+                        File usersFile = new File("files/" + currentUser + ".txt");
                         usersFile.createNewFile();
 
                         //Print to file
-                        FileWriter fileWriter = new FileWriter("files/"+currentUser+".txt", true);
+                        FileWriter fileWriter = new FileWriter("files/" + currentUser + ".txt", true);
                         PrintWriter printWriter = new PrintWriter(fileWriter);
                         printWriter.println(Arrays.asList(inputArray).toString()); // Mohsen's change
                         printWriter.println(dimensions);
@@ -110,21 +110,21 @@ public class Server {
                     }
                 }
 
-                else if(inputArray[0].equalsIgnoreCase("LIST")){
-                    if(isLoggedIn) {
+                else if (inputArray[0].equalsIgnoreCase("LIST")) {
+                    if (isLoggedIn) {
                         String listInformation = "";
                         File usersFile = new File("files/" + currentUser + ".txt");
 
                         //Case 1: -all flags
                         if (inputArray.length == 2 && inputArray[1].equals("-all")) {
-                            if(currentUser.equals("root")){
-                                String allInfo = getListInformation("root",  new File("files/root.txt"))   +
-                                                 getListInformation("john",  new File("files/john.txt"))   +
-                                                 getListInformation("sally", new File("files/sally.txt"))  +
-                                                 getListInformation("qiang", new File("files/qiang.txt"));
+                            if (currentUser.equals("root")) {
+                                String allInfo = getListInformation("root", new File("files/root.txt")) +
+                                        getListInformation("john", new File("files/john.txt")) +
+                                        getListInformation("sally", new File("files/sally.txt")) +
+                                        getListInformation("qiang", new File("files/qiang.txt"));
                                 outputToClient.writeUTF(allInfo);
                             }
-                            else{
+                            else {
                                 outputToClient.writeUTF("Error: you are not the root user");
                             }
                         }
@@ -135,19 +135,19 @@ public class Server {
                             outputToClient.writeUTF(listInformation);
                         }
                         //Case 3: no flags
-                        else if(inputArray.length == 1) {
+                        else if (inputArray.length == 1) {
                             outputToClient.writeUTF(getListInformation(currentUser, usersFile));
                         }
 
                         //Case 4: Command not correct
-                        else{
+                        else {
                             outputToClient.writeUTF("301 message format error");
                         }
                     }
                 }
 
-                else if(strReceived.equalsIgnoreCase("LOGOUT")){
-                    System.out.println("User "+currentUser+" logged out.");
+                else if (strReceived.equalsIgnoreCase("LOGOUT")) {
+                    System.out.println("User " + currentUser + " logged out.");
                     outputToClient.writeUTF("200 OK");
                     serverSocket.close();
                     socket.close();
@@ -156,7 +156,7 @@ public class Server {
                     return true;
                 }
 
-                else if(strReceived.equalsIgnoreCase("SHUTDOWN")) {
+                else if (strReceived.equalsIgnoreCase("SHUTDOWN")) {
                     System.out.println("Shutting down server...");
                     outputToClient.writeUTF("200 OK");
                     serverSocket.close();
@@ -168,20 +168,21 @@ public class Server {
                     outputToClient.writeUTF("300 invalid command");
                 }
             }//end server loop
-        }
-        catch(IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
-        }//end try-catch]
+        }
+
+        //end try-catch]
         return false;
     }//end createCommunicationLoop
 
     //This function gets the list information from the files and prints them out
-    public static String getListInformation(String name, File usersFile){
+    public static String getListInformation(String name, File usersFile) {
         String listInformation = "";
         try {
             listInformation = name + "\n";
             //If file exists and is not empty
-            if(usersFile.exists() && usersFile.length() > 0) {
+            if (usersFile.exists() && usersFile.length() > 0) {
                 Scanner s = new Scanner(usersFile);
                 while (s.hasNextLine()) {
                     String data = s.nextLine().substring(1).replaceFirst("]", "").replace(",", ""); //Get next Array of data
@@ -190,9 +191,11 @@ public class Server {
 
                     if (output.charAt(0) == 'E') { //If output is an Error Type
                         listInformation += "\t\t" + output + "\n";
-                    } else if (output.charAt(0) == 'R') { //If output is a Rectangle/Square
+                    }
+                    else if (output.charAt(0) == 'R') { //If output is a Rectangle/Square
                         listInformation += "\t\t" + "sides" + data.substring(8) + ": " + output + "\n";
-                    } else if (output.charAt(0) == 'C') { //If output is a Circle
+                    }
+                    else if (output.charAt(0) == 'C') { //If output is a Circle
                         listInformation += "\t\t" + "radius" + data.substring(8) + ": " + output + "\n";
                     }
                 }
@@ -201,53 +204,54 @@ public class Server {
             else {
                 listInformation += "\t\t" + "No interactions yet\n";
             }
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("File for List Information not Found");
         }
         return listInformation;
     }
 
     //Calculates the dimensions of the string array given and returns a string response of the answer
-    public static String calculateDimensions(String [] inputArray){
+    public static String calculateDimensions(String[] inputArray) {
         String response = "";
+        try {
+            if (inputArray[1].contains("-c")) {
+                if (inputArray.length == 3) {
+                    double area = Math.PI * Math.pow(Double.parseDouble(inputArray[2]), 2);
+                    double circumference = 2 * Math.PI * Double.parseDouble(inputArray[2]);
+                    response = "Circle's circumference is " + Math.round(circumference * 100.0) / 100.0 + " and area is " + Math.round(area * 100.0) / 100.0;
+                }
+                else if (inputArray.length == 2) {
+                    response = "ERROR: No radius found";
+                }
+                else {
+                    response = "301 message format error";
+                }
+            }
+            else if (inputArray[1].contains("-r")) {
+                if (inputArray.length == 3) {
+                    double area = Math.pow(Double.parseDouble(inputArray[2]), 2);
+                    double perimeter = 4 * Double.parseDouble(inputArray[2]);
+                    response = "Rectangle's perimeter is " + Math.round(perimeter * 100.0) / 100.0 + " and area is " + Math.round(area * 100.0) / 100.0;
+                }
+                else if (inputArray.length == 4) {
+                    double area = Double.parseDouble(inputArray[2]) * Double.parseDouble(inputArray[3]);
+                    double perimeter = 2 * Double.parseDouble(inputArray[2]) + 2 * Double.parseDouble(inputArray[3]);
+                    response = "Rectangle's perimeter is " + Math.round(perimeter * 100.0) / 100.0 + " and area is " + Math.round(area * 100.0) / 100.0;
+                }
+                else if (inputArray.length == 2) {
+                    response = "ERROR: No sides found";
+                }
+                else {
+                    response = "301 message format error";
+                }
 
-        if(inputArray[1].contains("-c")){
-            if(inputArray.length == 3){
-                double area = Math.PI * Math.pow(Double.parseDouble(inputArray[2]), 2);
-                double circumference = 2 * Math.PI * Double.parseDouble(inputArray[2]) ;
-                response = "Circle's circumference is " + Math.round(circumference * 100.0) /100.0 + " and area is " + Math.round(area * 100.0) /100.0;
             }
-            else if(inputArray.length == 2){
-                response= "ERROR: No radius found";
+            else {
+                response = "301 message format error";
             }
-            else{
-                response ="301 message format error";
-            }
+        } catch (Exception e) {
+            response = "301 message format error";
         }
-        else if(inputArray[1].contains("-r")){
-            if(inputArray.length == 3) {
-                double area = Math.pow(Double.parseDouble(inputArray[2]), 2);
-                double perimeter = 4 * Double.parseDouble(inputArray[2]);
-                response = "Rectangle's perimeter is " + Math.round(perimeter * 100.0) /100.0 + " and area is " + Math.round(area * 100.0) /100.0;
-            }
-            else if(inputArray.length == 4){
-                double area = Double.parseDouble(inputArray[2]) * Double.parseDouble(inputArray[3]);
-                double perimeter = 2 * Double.parseDouble(inputArray[2]) + 2 * Double.parseDouble(inputArray[3]);
-                response = "Rectangle's perimeter is " + Math.round(perimeter * 100.0) /100.0 + " and area is " + Math.round(area * 100.0) /100.0;
-            }
-            else if(inputArray.length == 2){
-                response= "ERROR: No sides found";
-            }
-            else{
-                response= "301 message format error";
-            }
-
-        }
-        else{
-            response= "301 message format error";
-        }
-
         return response;
     }
 
